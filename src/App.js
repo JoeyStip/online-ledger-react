4bc169bc1e4a5befe6214d7c4361933f0930a921
Joseph Stipich <stipichj@gmail.com>
2024-08-23 16:51:05 -0400

    EOD commit

--- src/App.js
+++ src/App.js
@@ -87,14 +87,14 @@ function Ledger(){
       }
     }
   })
-
+  
   const round=(n)=>{
     return Math.round(n*100)/100
   }
   
   const splitCost =(e)=>{
     const split = e.target.value/3;
-    console.log(e.target.id);
+    //console.log(e.target.id);
     let valuesCopy = structuredClone(values);
     valuesCopy["recurringCosts"][e.target.id]["total"] = round(e.target.value);
     for(let x = 0; x < members.length; x++){
@@ -114,23 +114,19 @@ function Ledger(){
       valuesDeepCopy["recurringCosts"]["totals"][members[x]] = round(total);
       total = 0;
     }
-
+    //overall total
     for(let y = 0; y < costs["recurringCosts"].length; y++){
-      total = total + parseInt(valuesDeepCopy["recurringCosts"][costs["recurringCosts"][y]]["total"])
+      total = total + valuesDeepCopy["recurringCosts"][costs["recurringCosts"][y]]["total"]
     }
     if(total>0){
       valuesDeepCopy["recurringCosts"]["totals"]["overall"] = round(total);
     } else {
       valuesDeepCopy["recurringCosts"]["totals"]["overall"] = 0.00;
     }
-    
-      
     updateBalances(e, valuesDeepCopy)
   }
 
   const updateBalances =(e, valuesDeepCopy)=>{
-    // console.log(costsCopy); 
-    // console.log(e.target.value)
     for(let x = 0; x < members.length; x++){
       let totalCosts = 0
       for(let y = 0; y < costs["recurringCosts"].length; y++){
@@ -139,13 +135,33 @@ function Ledger(){
       valuesDeepCopy["Balances"]["ADD: Total Costs"][members[x]] = round(totalCosts);
       totalCosts = 0
     }
-
     setValues(valuesDeepCopy)
   }
 
+  const [ledgerElements, setLedgerElements] = useState([
+    <span>water/sewer</span>,
+    <input onChange={splitCost} id="water/sewer" type="text" placeholder="0.00"/>,
+    <input type="date" />,
+    <span className="amount Brad">{values["recurringCosts"]["water/sewer"]["Brad"]}</span>,
+    <span className="amount Carson">{values["recurringCosts"]["water/sewer"]["Carson"]}</span>,
+    <span className="amount Sean">{values["recurringCosts"]["water/sewer"]["Sean"]}</span>,
+    <span>electric</span>,
+    <input onChange={splitCost} id="electric" type="text" placeholder="0.00"/>,
+    <input type="date" />,
+    <span className="amount Brad">{values["recurringCosts"]["electric"]["Brad"]}</span>,
+    <span className="amount Carson">{values["recurringCosts"]["electric"]["Carson"]}</span>,
+    <span className="amount Sean">{values["recurringCosts"]["electric"]["Sean"]}</span>,
+    <span>natural gas</span>,
+    <input onChange={splitCost} id="natural gas" type="text" placeholder="0.00"/>,
+    <span>8/20/2024</span>,
+    <span className="amount Brad">{values["recurringCosts"]["natural gas"]["Brad"]}</span>,
+    <span className="amount Carson">{values["recurringCosts"]["electric"]["Carson"]}</span>,
+    <span className="amount Sean">{values["recurringCosts"]["natural gas"]["Sean"]}</span>
+  ])
+
   const addCost =()=>{
-    
-  }
+    setLedgerElements();
+  } 
 
   return (
     <div id="ledger">
@@ -160,31 +176,15 @@ function Ledger(){
       <div id="recurringCosts" className="ledgerSection">
         <span className="sectionHeader">Recurring Costs</span>
         <div className="sectionTable">
-          <span>water/sewer</span>
-          <input onChange={splitCost} id="water/sewer" type="text" placeholder="0.00"/>
-          <input type="date" />
-          <span className="amount line1 Brad">{values["recurringCosts"]["water/sewer"]["Brad"]}</span>
-          <span className="amount line1 Carson">{values["recurringCosts"]["water/sewer"]["Carson"]}</span>
-          <span className="amount line1 Sean">{values["recurringCosts"]["water/sewer"]["Sean"]}</span>
-          <span>electric</span>
-          <input onChange={splitCost} id="electric" type="text" placeholder="0.00"/>
-          <span>8/20/2024</span>
-          <span className="amount line2 Brad">{values["recurringCosts"]["electric"]["Brad"]}</span>
-          <span className="amount line2 Carson">{values["recurringCosts"]["electric"]["Carson"]}</span>
-          <span className="amount line2 Sean">{values["recurringCosts"]["electric"]["Sean"]}</span>
-          <span>natural gas</span>
-          <input onChange={splitCost} id="natural gas" type="text" placeholder="0.00"/>
-          <span>8/20/2024</span>
-          <span className="amount line3 Brad">{values["recurringCosts"]["natural gas"]["Brad"]}</span>
-          <span className="amount line3 Carson">{values["recurringCosts"]["natural gas"]["Carson"]}</span>
-          <span className="amount line3 Sean">{values["recurringCosts"]["natural gas"]["Sean"]}</span>
+          {ledgerElements}
           <span className="totals">Total</span>
-          <span className="amount totals">{values["recurringCosts"]["totals"]["overall"]}</span>
+          <span id="overallRecurringTotal" className="amount totals">{values["recurringCosts"]["totals"]["overall"]}</span>
           <span></span>
           <span className="amount totals Brad">{values["recurringCosts"]["totals"]["Brad"]}</span>
           <span className="amount totals Carson">{values["recurringCosts"]["totals"]["Carson"]}</span>
           <span className="amount totals Sean">{values["recurringCosts"]["totals"]["Sean"]}</span>
           <button onClick={addCost}>+</button>
+          <div></div>
         </div>
       </div>
       <div id="otherCosts" className="ledgerSection">
